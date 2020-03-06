c9da608ce0bcc8848249f2cf4ba7684edaef2b23
Zachary Wolfson <zwolfson@hubspot.com>
2020-03-06 11:35:23 -0500

    Add OAuth Steps

--- web_service/src/server.js
+++ web_service/src/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const hubspot = require("@hubspot/api-client");
 const bodyParser = require("body-parser");
 const path = require("path");
 const axios = require("axios");
@@ -13,7 +13,47 @@ const ideaRouter = require("./Ideas.API");
 const app = express();
 var apiRouter = express.Router();
 
+const { CLIENT_ID, BASE_URL, SCOPES, CLIENT_SECRET } = process.env;
+
+const REDIRECT_URL = `${BASE_URL}/oauth/callback`;
+const hubspotClient = new hubspot.Client();
+
 app.use(bodyParser.json());
+
+app.get("/oauth/connect", async (req, res) => {
+  const authorizationUrl = hubspotClient.oauth.getAuthorizationUrl(
+    CLIENT_ID,
+    REDIRECT_URL,
+    SCOPES
+  );
+
+  res.redirect(authorizationUrl);
+});
+
+app.get("/oauth/callback", async (req, res, next) => {
+  const { code } = req.query;
+  try {
+    const tokensResponse = await hubspotClient.oauth.defaultApi.createToken(
+      "authorization_code",
+      code,
+      REDIRECT_URL,
+      CLIENT_ID,
+      CLIENT_SECRET
+    );
+    const { accessToken, refreshToken, expiresIn } = tokensResponse.body;
+    const expiresAt = new Date(Date.now() + expiresIn);
+
+    const accountInfo = await Account.findOneAndUpdate(
+      { accountId: 1 },
+      { accessToken, refreshToken, expiresAt },
+      { new: true, upsert: true }
+    );
+
+    res.redirect("/");
+  } catch (err) {
+    next(err);
+  }
+});
 
 apiRouter.use("/users", userRouter);
 apiRouter.use("/ideas", ideaRouter);
