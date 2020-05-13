3d395a9e51d304fc8fb60c139209fa663d719dba
Zachary Wolfson <zwolfson@hubspot.com>
2020-05-13 16:48:30 -0400

    sending idea as timeline event

--- web_service/src/Ideas.API.js
+++ web_service/src/Ideas.API.js
@@ -2,11 +2,23 @@ const express = require("express");
 const Idea = require("./Ideas.model");
 var ideaRouter = express.Router();
 
+const createTimeLineEvent = async (idea) => {
+  const accessToken = await getAccessToken(1);
+  try {
+    await axios.post(
+      `http://hubspot_service:8080/api/timeline/${accessToken}`,
+      {
+        idea,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 ideaRouter.get("/", async (req, res, next) => {
   try {
-    const allIdeas = await Idea.find({})
-      .populate("author")
-      .exec();
+    const allIdeas = await Idea.find({}).populate("author").exec();
 
     res.send(allIdeas);
   } catch (err) {
@@ -21,6 +33,7 @@ ideaRouter.post("/", async (req, res, next) => {
     const dbResponse = await Idea.create(idea);
     const populatedIdea = await dbResponse.populate("author").execPopulate();
     res.send(populatedIdea);
+    createTimeLineEvent(populatedIdea);
   } catch (err) {
     next(err);
   }
