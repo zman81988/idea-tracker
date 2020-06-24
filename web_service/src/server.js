c02588a0d2404126e18efe36c47cccc9b2d8e548
Zachary Wolfson <zwolfson@hubspot.com>
2020-06-24 11:19:02 -0400

    dual start bug fix

--- web_service/src/server.js
+++ web_service/src/server.js
@@ -283,13 +283,6 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.toString());
 });
 
-console.log("process environment", process.env.NODE_ENV);
-app.listen(process.env.PORT || 8080, () => {
-  connectDb().then(() => {
-    console.log("database connected");
-  });
-});
-
 const options = {
   key: fs.readFileSync(
     path.resolve(__dirname, "../ssl/idea-tracker-tutorial.key")
