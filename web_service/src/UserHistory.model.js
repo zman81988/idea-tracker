c246ad9b4ebe89882efd679b00738843577c9b54
Zachary Wolfson <zwolfson@hubspot.com>
2020-07-01 17:05:30 -0400

    fixing default values

--- web_service/src/UserHistory.model.js
+++ web_service/src/UserHistory.model.js
@@ -2,14 +2,26 @@ const mongoose = require("mongoose");
 
 const propertyHistorySchema = new mongoose.Schema({
   value: { type: String, default: "" },
-  whenModified: { type: Date, default: Date.now() }
+  whenModified: { type: Date, default: Date.now() },
 });
 
 const userHistorySchema = new mongoose.Schema({
-  firstNameHistory: [propertyHistorySchema],
-  lastNameHistory: [propertyHistorySchema],
-  rankHistory: [propertyHistorySchema],
-  emailHistory: [propertyHistorySchema]
+  firstNameHistory: {
+    type: [propertyHistorySchema],
+    default: [{ value: "", whenModified: Date.now() }],
+  },
+  lastNameHistory: {
+    type: [propertyHistorySchema],
+    default: [{ value: "", whenModified: Date.now() }],
+  },
+  rankHistory: {
+    type: [propertyHistorySchema],
+    default: [{ value: "", whenModified: Date.now() }],
+  },
+  emailHistory: {
+    type: [propertyHistorySchema],
+    default: [{ value: "", whenModified: Date.now() }],
+  },
 });
 
 module.exports = userHistorySchema;
